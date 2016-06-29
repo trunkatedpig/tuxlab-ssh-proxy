8f9acc634695b82ba875a346a655f748a11702e2
DerekTBrown <derek@allderek.com>
2016-06-29 00:56:23 -0400

    Created Proxyfile

--- src/proxy.js
+++ src/proxy.js
@@ -1,74 +1,56 @@
-/* TuxLab - Reverse Proxy
-   This server is a combination reverse SSH proxy and
-   web socket to SSH bridge, allowing users to connect to
-   their respective docker containers.
+/**
+  SSH Tunnel Example using RedRouter
+**/
+
+// Imports
+var fs = require('fs');
+
+// Import RedRouter Core
+var redrouter = require('redrouter').create;
+
+// Import RedRouter Components
+var backend_etcd = require('redrouter.backend.etcd');
+var agent_ssh = require('redrouter.agent.ssh-proxy');
+var agent_wetty = require('redrouter.agent.wetty');
+var resolver_ssh = require('redrouter.resolver.ssh');
+
+/*
+  Define a RedRouter Instance
 */
-
-/* Read Settings */
-  var fs = require('fs');
-  var settings = JSON.parse(fs.readFileSync("/root/settings.json"));
-
-/* Create WebSSH Listener */
-
-    // Create Wetty server
-      var pty = require('pty.js');
-      var wetty = require('http').createServer(handler).listen(settings.webssh_port, function() {
-        console.log('listening for WebSSH Connections on '+settings.webssh_port);
-      });
-      var io = require('socket.io')(wetty,{path : '/ssh'});
-
-    // HTTP Request Handler
-      function handler (req,res){
-        // Set Headers
-          if (res.headers.host = "localhost"){
-            res.setHeader("Access-Control-Allow-Origin", "null");
-          }
-          else{
-            res.setHeader("Access-Control-Allow-Origin", "*");
-          }
-          res.setHeader("Access-Control-Allow-Credentials","true")
-
-        // Write Results
-          res.writeHead(200);
-          res.end();
+var proxy = new redrouter({
+  ssl : {
+    key : fs.readFileSync('/root/local/host.key'),
+    cert : fs.readFileSync('/root/local/host.key.pub')
+  },
+  backend : {
+    constructor: backend_etcd,
+    options: {
+      host: "172.17.0.1:2379",
+      etcd_conn_opts: {}
+    }
+  },
+  resolvers: [
+    { constructor: resolver_ssh,
+      options: {
+        defaults: {
+          allowed_auth: ['password']
+        }
       }
-
-    // WebSSH Request Handler
-      io.set('origins', '*:80');
-      io.on('connection', function(socket){
-        var request = socket.request;
-
-        // Validate SSH Request
-        var request_domain = request.headers.domain;
-        if (request_domain.indexOf(webssh_root, request_domain - webssh_root.length) !== -1){
-            var username = request_domain.replace("."+request_domain,"");
-            var ssh_hostname = username + "@" + request_domain;
-        }
-        else{
-            socket.emit('exception', {errorMessage: 'Invalid Host'})
-        }
-
-        var term = pty.spawn('ssh', [ssh_hostname,"-p",settings.ssh_port],{
-          name : 'xterm-256color',
-          cols: 80,
-          rows: 30
-        });
-
-        // Set Data Passthrough
-        console.log((new Date()) + " PID=" + term.pid)
-        term.on('data', function(data) {
-            socket.emit('output', data);
-        });
-        term.on('exit', function(code) {
-            console.log((new Date()) + " PID=" + term.pid + " ENDED")
-        });
-        socket.on('resize', function(data) {
-            term.resize(data.col, data.row);
-        });
-        socket.on('input', function(data) {
-            term.write(data);
-        });
-        socket.on('disconnect', function() {
-            term.end();
-        });
-      })
+    }
+  ],
+  agents: [
+    {
+      constructor: agent_wetty,
+      options: {
+        host: 'localhost',
+        port: 80
+      }
+    },
+    { constructor: agent_ssh,
+      options: {
+        host: 'localhost',
+        port: 22
+      }
+    }
+  ]
+});
